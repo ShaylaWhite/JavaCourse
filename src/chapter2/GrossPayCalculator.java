b32344ace85d8312adbc2b75077076f533abad15
Shayla White <45671858+ShaylaWhite@users.noreply.github.com>
2021-05-22 16:48:07 -0700

    SW:algorthim instructions

--- src/chapter2/GrossPayCalculator.java
+++ src/chapter2/GrossPayCalculator.java
@@ -6,6 +6,9 @@ public class GrossPayCalculator {
     //main method the starting point of the program
 
     public static void main(String arg[]) {
-
+         //get the number of hours worked
+        // get the hourly pay rate
+        //mutilpy hours and pay rate
+        //display results
     }
 }
