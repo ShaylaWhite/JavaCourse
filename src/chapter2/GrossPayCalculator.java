b120ba8374728835162a7ba42c333f5e8992be4e
Shayla White <45671858+ShaylaWhite@users.noreply.github.com>
2021-05-22 16:40:21 -0700

    SW:main method

--- src/chapter2/GrossPayCalculator.java
+++ src/chapter2/GrossPayCalculator.java
@@ -1,4 +1,11 @@
 package chapter2;
 
 public class GrossPayCalculator {
+    //block of code
+    //default method
+    //main method the starting point of the program
+
+    public static void main(String arg[]) {
+
+    }
 }
