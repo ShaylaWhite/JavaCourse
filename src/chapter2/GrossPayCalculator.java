f26d787f156a44624f1fee119c3f083df4735e27
Shayla White <45671858+ShaylaWhite@users.noreply.github.com>
2021-05-22 17:03:22 -0700

    SW:variable declartions

--- src/chapter2/GrossPayCalculator.java
+++ src/chapter2/GrossPayCalculator.java
@@ -8,16 +8,17 @@ public class GrossPayCalculator {
     //main method the starting point of the program
 
     public static void main(String arg[]) {
-         //1get the number of hours worked
+         //1.get the number of hours worked
         System.out.println("Enter the number of the hours the employee worked. ");
         Scanner scanner = new Scanner(System.in);
-        scanner.nextInt();
+        int hours = scanner.nextInt(); // variable declaration
 
-        //2 get the hourly pay rate
+        //2. get the hourly pay rate
         System.out.println("Enter the employee's pay rate");
-        scanner.nextDouble();
+        double rate = scanner.nextDouble(); //variable declaration
 
-        //3multiply hours and pay rate
+        //3. multiply hours and pay rate
+
         //display results
 
     }
