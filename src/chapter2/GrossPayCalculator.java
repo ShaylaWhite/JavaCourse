1ddef86b6928e6bc093953d58345f07bd4915f14
Shayla White <45671858+ShaylaWhite@users.noreply.github.com>
2021-05-22 17:00:31 -0700

    SW:algorithm step one and step 2; receive input and import class scanner

--- src/chapter2/GrossPayCalculator.java
+++ src/chapter2/GrossPayCalculator.java
@@ -1,14 +1,24 @@
 package chapter2;
 
+import java.util.Scanner;
+
 public class GrossPayCalculator {
     //block of code
     //default method
     //main method the starting point of the program
 
     public static void main(String arg[]) {
-         //get the number of hours worked
-        // get the hourly pay rate
-        //mutilpy hours and pay rate
+         //1get the number of hours worked
+        System.out.println("Enter the number of the hours the employee worked. ");
+        Scanner scanner = new Scanner(System.in);
+        scanner.nextInt();
+
+        //2 get the hourly pay rate
+        System.out.println("Enter the employee's pay rate");
+        scanner.nextDouble();
+
+        //3multiply hours and pay rate
         //display results
+
     }
 }
